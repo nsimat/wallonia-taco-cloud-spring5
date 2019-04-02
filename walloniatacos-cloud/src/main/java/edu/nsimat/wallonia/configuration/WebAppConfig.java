cfb687e449c22e077419519bca8d125544c5a112
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-04-02 02:47:29 +0200

    Refactoring WebConfig by renaming it WebAppConfig

--- walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/configuration/WebAppConfig.java
+++ walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/configuration/WebAppConfig.java
@@ -1,11 +1,11 @@
-package edu.nsimat.wallonia;
+package edu.nsimat.wallonia.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebAppConfig implements WebMvcConfigurer{
 
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
