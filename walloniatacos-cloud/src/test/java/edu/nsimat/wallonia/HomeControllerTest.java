f0e332d463f69adc89d3d562c3066cbed5d9c32c
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-04-02 02:28:16 +0200

    Adding a configuration class that implements WebMvcConfigurer.

--- walloniatacos-cloud/src/test/java/edu/nsimat/wallonia/HomeControllerTest.java
+++ walloniatacos-cloud/src/test/java/edu/nsimat/wallonia/HomeControllerTest.java
@@ -13,10 +13,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import edu.nsimat.wallonia.controller.HomeController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 public class HomeControllerTest {
 	
 	@Autowired
