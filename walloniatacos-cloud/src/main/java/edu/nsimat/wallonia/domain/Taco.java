fbed880722202f7a62b617bdd319c05fdd0cf709
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-04-04 03:59:41 +0200

    Adding error.html to handle error page and modifying Order class

--- walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/domain/Taco.java
+++ walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/domain/Taco.java
@@ -1,5 +1,6 @@
 package edu.nsimat.wallonia.domain;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -10,9 +11,15 @@ import lombok.Data;
 @Data
 public class Taco {
 	
+	private Long id;
+	
+	private Date createdAt;
+	
 	@NotNull
 	@Size(min=5, message="Name must be at least 5 characters long.")
 	private String name;
+	
+	private Date crreatedAt;
 	
 	@NotNull(message="You must choose at least 1 ingredient.")
 	@Size(min=1)
