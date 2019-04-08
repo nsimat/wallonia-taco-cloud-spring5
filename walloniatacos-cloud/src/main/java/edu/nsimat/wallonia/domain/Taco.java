2e5e8002bfedb26ad8ccb6b4dafbd35e37b750c5
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-04-08 14:48:26 +0200

    Adding error page and an id to the Taco class

--- walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/domain/Taco.java
+++ walloniatacos-cloud/src/main/java/edu/nsimat/wallonia/domain/Taco.java
@@ -19,8 +19,6 @@ public class Taco {
 	@Size(min=5, message="Name must be at least 5 characters long.")
 	private String name;
 	
-	private Date crreatedAt;
-	
 	@NotNull(message="You must choose at least 1 ingredient.")
 	@Size(min=1)
 	private List<String> ingredients;
